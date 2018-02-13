a633431ada0f9c17f67ddba1b9411ba9823b32b2
DuckSoft <ducksoft@foxmail.com>
2018-02-13 18:55:53 +0800

    Pair: 初步实现
    Close #4

---
+++ src/org/chembar/glockparse/Pair.kt
@@ -0,0 +1,15 @@
+package org.chembar.glockparse
+
+/** Pair
+ * @author DuckSoft
+ *
+ * @param L Pair左侧类型
+ * @param R Pair右侧类型
+</R></L> */
+class Pair<L, R>(var l: L, var r: R) {
+    // 用于Kotlin自动解构
+    operator fun component1() = l
+    operator fun component2() = r
+    // 用于调试输出
+    override fun toString() = "($l,$r)"
+}
